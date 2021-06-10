e45b08dd88b40b4fe9f34d9778329b11a25526ca
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-06-09 23:17:35 -0400

    Got log i  to work displays username and balance hurray

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,10 +1,56 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState, useEffect } from "react"
 
 const Dashboard = ({setAuth}) => {
+
+    const [name, setName] = useState("")
+    const [balance, setBalance] = useState("")
+
+    async function getName() {
+
+        try {
+        const response = await fetch("https://moneymattersbackend.herokuapp.com/dashboard", {
+            method:"GET",
+            headers: {token: localStorage.token}
+        })
+        console.log(response)
+        const parseResponse = await response.json()
+        console.log(parseResponse)
+        setName(parseResponse.user_name)
+
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    async function getBalance() {
+
+        try {
+        const response = await fetch("https://moneymattersbackend.herokuapp.com/dashboard", {
+            method:"GET",
+            headers: {token: localStorage.token}
+        })
+        console.log(response)
+        const parseResponse = await response.json()
+        console.log(parseResponse)
+        setBalance(parseResponse.account_balance)
+
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+
+useEffect(() => {
+    getName()
+    getBalance()
+})
+
+
     return (
         <Fragment>
-        <h1>Dashboard</h1>
-        <button onClick={()=> setAuth(false)}>Log Out</button>
+        <h1>Dashboard {name}</h1>
+        <h2>balance ${balance}</h2>
+
         </Fragment>
     )
 }
